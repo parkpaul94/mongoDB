280112f1b5b3ee94baff07baeeb001c206b42374
Paul Park <park.paul.94@gmail.com>
2018-08-12 23:38:38 -0400

    made mongoDB Application

--- server.js
+++ server.js
@@ -1,106 +1,54 @@
-  var cheerio = require("cheerio");
-  var request = require("request");
-  var express = require("express");
-  var mongojs = require("mongojs");
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+require('dotenv').config();
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
 
-  var app = express();
-  var PORT = process.env.PORT || 3000;
-  var databaseUrl = "momgoDB";
-  var collections = ["scrapeData"];
+var PORT = process.env.PORT || 8080;
 
-  app.use(express.static("public"));
-  // Hook mongojs configuration to the db variable
-  var db = mongojs(databaseUrl, collections);
-  db.on("error", function(error) {
-    console.log("Database Error:", error);
-  });
+// Initialize Express
+var app = express();
 
-  // Main route (simple Hello World Message)
-  app.get("/", function(req, res) {
-    res.send(index.html);
-  });
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-  // Retrieve data from the db
-  app.get("/all", function(req, res) {
-    // Find all results from the scrapeData collection in the db
-    db.scrapeData.find({}, function(error, found) {
-      console.log(found);
-      //throw any errors to the console
-      if (error) {
-        console.log(error);
-      }
-      //if there are no errors, send the data to the browser
-      else {
-        res.json(found);
-      }
-    })
-  })
+// Serve static content
+app.use(express.static("public"));
 
-  app.get("/scrape", function(req, res) {
-    scrape().then(results => {
-      results.forEach(result => {
-        db.scrapeData.insert(result)
-      })
-      res.redirect("/all");
-    })
-  })
-  // function scrape(){
-  //   return new Promise ((resolve, reject)=>{
-  //     request("http://nytimes.com/section/health", function(error, response, html){
-  //       var $= cheerio.load(html);
-  //       var results = [];
-  //       $(".story-body").each(function(i, element){
-  //         var title = $(element).find("h2.headline").text().trim();
-  //         var summary = $(element).find("p.summary").text().trim();
-  //         var url = $(element).find("a").attr("href").trim();
-  //         results.push({
-  //           Title: title,
-  //           Summary: summary,
-  //           URL: url
-  //         });
-  //       });
-  //       console.log(results);
-  //       resolve(results);
-  //     })
-  //   })
-  // }
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-function scrape() {
-  return new Promise ((resolve, reject) => {
-    request("https://www.imdb.com/movies-in-theaters/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=2413b25e-e3f6-4229-9efd-599bb9ab1f97&pf_rd_r=FZCPVB87325RAQSM8CWE&pf_rd_s=right-2&pf_rd_t=15061&pf_rd_i=homepage&ref_=hm_otw_hd", function(error, response, html) {
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-  var $ = cheerio.load(html);
-  var results = [];
+// Import routes and give the server access to them.
+var routes = require("./controllers/scraper_controller.js");
 
-  $(".list_item").each(function(i, element) {
-    var movie_name = $(element).find('h4').children().attr('title');
-    var actors = $(element).find('[itemprop="actors"]');
+app.use("/", routes);
 
-    const actorArray = [];
-    actors.each((i, el) => actorArray.push($(el).text().split('\n')[2]));
-    // var actor_list = actors.text().replace(/  /g, '').split('\n').filter(Boolean);
+const db = process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
+var promise = mongoose.connect(db, { useNewUrlParser: true }); //connect to mongoose database
+mongoose.connection.on('connected', () => {     //tell us if we are connected
+console.log('Connected to database mongodb://localhost:27017/test')
+});
 
-    var genre = $(element).find('[itemprop="genre"]');
-    const genreArray = [];
-    genre.each((i, el) => genreArray.push($(el).text()));
-    // var genre = genre_list.text();
-    var time = $(element).find('[itemprop="duration"]').text();
+mongoose.connection.on('error', (error) => {     //tell the error if it occurs
+console.log('Database Error'+ error)
+});
 
-    results.push({
-      Movie_Title: movie_name,
-      Movie_Genre: genreArray,
-      Movie_Duration: time,
-      Actors: actorArray,
-
-    });
-  });
-  console.log(results);
-  resolve(results);
-  });
-})
-}
-
-  // Listen on port 3000
-  app.listen(PORT, function() {
-    console.log("App running on port 3000!");
-  });
+// Listen on port 3000
+app.listen(PORT, function() {
+  console.log("App running on PORT " + PORT);
+});
